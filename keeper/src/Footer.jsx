5e29d727e58dfd50a8508f790fd6221efbabe7e4
Felix Asare <asarefelix440@gmail.com>
2020-05-31 12:06:56 -0500

    adding react files

---
+++ keeper/src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+    let date = new Date()
+    let year  = date.getFullYear()
+    
+
+
+    return <footer>
+        <p>Copyright © {year}</p>
+    </footer>
+}
+
+export default Footer
---
+++ keeper/src/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note (){
+    return <div className = "note">
+        <h1>This is title</h1>
+        <p>This is notes</p>
+    </div>
+}
+
+export default Note
